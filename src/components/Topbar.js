b12ba54a73eca976606ec6c397111bff78fa3a22
Haiyang Hong <hong961127@gmail.com>
2020-09-03 21:31:58 -0700

    Finish Topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,13 +3,22 @@ import { makeStyles } from "@material-ui/core";
 
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    marginTop: theme.spacing(5.5),
     height: 90,
-    background: "#ddd",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    paddingLeft: theme.spacing(8.875),
+    paddingRight: theme.spacing(6.25),
   },
   formControl: {
     width: "21.5rem",
@@ -31,13 +40,32 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiInputLabel-outlined": {
       transform: `translate(58px, 20px)`,
       ...theme.typography.headerSearch,
-
       "&.MuiInputLabel-shrink": {
-        transform: `translate(16px, -3px) scale(0.75)`,
+        transform: `translate(16px, -5px) scale(0.75)`,
         color: `#3f51b5`,
         fontWeight: 700,
       },
     },
+  },
+  right: {
+    display: "flex",
+    alignItems: "flex-start",
+  },
+  rightIcons: {
+    color: theme.palette.common.black,
+    width: 22,
+    height: 22,
+  },
+  dateTime: {
+    marginLeft: theme.spacing(6.875),
+  },
+  time: {
+    ...theme.typography.headerTime,
+    lineHeight: 1.2,
+  },
+  date: {
+    ...theme.typography.headerDate,
+    textAlign: "right",
   },
 }));
 
@@ -54,6 +82,18 @@ const Topbar = () => {
           variant="outlined"
           label="Search"
         />
+      </Box>
+      <Box className={classes.right}>
+        <IconButton>
+          <MailOutlineIcon className={classes.rightIcons} />
+        </IconButton>
+        <IconButton>
+          <NotificationsNoneIcon className={classes.rightIcons} />
+        </IconButton>
+        <Box className={classes.dateTime}>
+          <Typography className={classes.time}>17:30</Typography>
+          <Typography className={classes.date}>18 August</Typography>
+        </Box>
       </Box>
     </Box>
   );

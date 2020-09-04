7fbb15ddfaaa6e7d4c6fc003071114690e2d856c
Haiyang Hong <hong961127@gmail.com>
2020-09-03 19:45:30 -0700

    Setup Mui, add Typography in theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+
+import theme from "./theme";
+
+import App from "./components/App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/theme.js
@@ -0,0 +1,76 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const black = "#0a0e1c";
+const blue = "#4c6ae6";
+const red = "#ff6161";
+const green = "#90c335";
+const purple = "#985bff";
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      black,
+      blue,
+      red,
+      green,
+      purple,
+    },
+  },
+  typography: {
+    fontFamily: "muli, sans-serif",
+    headerTime: {
+      fontSize: 36,
+      color: black,
+    },
+    headerDate: {
+      fontSize: 14,
+      color: black,
+    },
+    headerSearch: {
+      fontSize: 16,
+      color: black,
+      opacity: 0.5,
+    },
+    sidebarBrand: {
+      fontSize: 33,
+      fontWeight: 900,
+      color: black,
+    },
+    sidebarText: {
+      fontSize: 16,
+      color: black,
+    },
+    sidebarName: {
+      fontSize: 16,
+      fontWeight: "700",
+      color: black,
+    },
+    mainTitle: {
+      fontSize: 14,
+      fontWeight: 700,
+      color: black,
+    },
+    mainProjectName: {
+      fontSize: 18,
+      fontWeight: 700,
+      color: black,
+    },
+    mainProjectInitial: {
+      fontSize: 20,
+      fontWeight: 900,
+    },
+    taskTitle: {
+      fontSize: 16,
+      color: black,
+    },
+    taskType: {
+      fontSize: 12,
+      fontWeight: 700,
+      color: "#fff",
+    },
+    small: {
+      fontSize: 12,
+      color: black,
+    },
+  },
+});
